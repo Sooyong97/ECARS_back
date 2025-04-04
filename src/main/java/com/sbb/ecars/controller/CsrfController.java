22dd1f70b3a7ffac3c670360dd12c2f39d22823a
Sooyong97 <01071236398y@gmail.com>
2025-04-05 01:40:50 +0900

    fix: CSRF 토큰 발급 수정

--- src/main/java/com/sbb/ecars/controller/CsrfController.java
+++ src/main/java/com/sbb/ecars/controller/CsrfController.java
@@ -1,4 +1,5 @@
 package com.sbb.ecars.controller;
+import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CsrfController {
 
-    @GetMapping("/csrf-token/")
-    public ResponseEntity<CsrfToken> getCsrfToken(HttpServletRequest request) {
-        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        return ResponseEntity.ok(csrfToken);
+    @GetMapping("/csrf-token")
+    public ResponseEntity<Map<String, String>> getCsrfToken(CsrfToken csrfToken) {
+
+        return ResponseEntity.ok(Map.of("token", csrfToken.getToken()));
     }
 }
