f708531cf219c0f48491b05cb25470e1e6ef7107
Sooyong97 <01071236398y@gmail.com>
2025-04-05 01:40:00 +0900

    refactor: PostDto 형식 수정

--- src/main/java/com/sbb/ecars/dto/PostDto.java
+++ src/main/java/com/sbb/ecars/dto/PostDto.java
@@ -3,6 +3,7 @@ package com.sbb.ecars.dto;
 import com.sbb.ecars.domain.Post;
 import lombok.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Getter
@@ -15,15 +16,16 @@ public class PostDto {
     private Map<String, Object> fields;
 
     public static PostDto fromEntity(Post post) {
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("user_id", post.getUserId());
+        fields.put("title", post.getTitle());
+        fields.put("content", post.getContent());
+        fields.put("created_at", post.getCreatedAt());
+        fields.put("file_path", post.getFilePath());
+
         return PostDto.builder()
                 .pk(post.getId())
-                .fields(Map.of(
-                    "user_id", post.getUserId(),
-                    "title", post.getTitle(),
-                    "content", post.getContent(),
-                    "created_at", post.getCreatedAt(),
-                    "file_path", post.getFilePath()
-                ))
+                .fields(fields)
                 .build();
     }
 }
